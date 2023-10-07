b7fa76d2ae51cc76d2da39127972c7d055bdee8b
Md Sadat Khan <sadatcse@gmail.com>
2023-10-07 18:52:56 +0600

    Install Firebase

--- src/componenents/Page/About.jsx
+++ src/componenents/Page/About.jsx
@@ -2,10 +2,45 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2>About</h2>
+        <div className="bg-gray-100 p-8 mx-auto max-w-screen-xl p-4">
+            <h2 className="text-2xl font-bold mb-4">About Us:</h2>
+            <p className="text-gray-700 mb-8">
+                At S Wedding Planner, our journey began in 2020, specializing in catering. Driven by popular demand, we gradually expanded into comprehensive wedding event planning, logistics, and management. Our reputation quickly flourished, thanks to our innovation, professionalism, and adaptability. We take pride in meeting the unique needs of our clients and maintaining a relentless pursuit of perfection. In 2023, we rebranded ourselves as S Wedding Planner to better reflect our evolving services. Over the years, we have successfully catered to a wide range of events, from intimate home weddings to grand VIP extravaganzas, ensuring memorable experiences for all.
+            </p>
+
+            <h3 className="text-xl font-bold mb-4">Why Choose Us:</h3>
+            <ul className="list-disc pl-4 mb-8">
+                <li className="mb-2">Bring Your Dream Wedding Vision to Life: We specialize in transforming your dream wedding into a spectacular reality, offering creativity and meticulous planning.</li>
+                <li className="mb-2">Manage Your Budget: We are adept at managing budgets, ensuring you get the most out of your wedding experience.</li>
+                <li className="mb-2">Problem Solving Pros: Count on us to tackle challenges and provide effective solutions.</li>
+                <li className="mb-2">Ensure No Key Detail is Overlooked: We pay meticulous attention to every detail, leaving no aspect to chance.</li>
+                <li className="mb-2">Ensuring You Stay On Track: We keep your wedding planning on course, so you can focus on celebrating.</li>
+                <li className="mb-2">Take Away Unwanted Stress: Our expertise minimizes stress, allowing you to enjoy the journey.</li>
+                <li className="mb-2">Timeline Professionals: We excel in keeping your event timeline seamless and efficient.</li>
+                <li className="mb-2">Let Your Friends and Family Enjoy Your Wedding Day with You: We handle the logistics, so your loved ones can fully immerse themselves in the celebration.</li>
+            </ul>
+
+            <h3 className="text-xl font-bold mb-4">Our Promise to You:</h3>
+            <p className="text-gray-700 mb-8">
+                At S Wedding Planner, we promise to deliver professional event management services in a creative, innovative, and cost-effective manner. We prioritize fulfilling your specific aims and exceeding your expectations. We provide a "value-added" service honed through years of experience with unique events. Expect seamless event management complemented by exceptional cuisine and décor that will leave you and your guests incredibly satisfied.
+            </p>
+
+            <h3 className="text-xl font-bold mb-4">Our Dedicated and Professional Team:</h3>
+            <p className="text-gray-700 mb-8">
+                Our dedicated and professional team stands with you every step of the way. Attention to detail, teamwork, and fulfilling your wishes drive our success in event management. Our team includes Sales and Event Managers, Event Coordinators, and experienced Event Managers. We ensure meticulous planning, allowing you to be a guest at your own event and savor every moment. Hospitality isn't just a word; it's the core value that defines our company.
+            </p>
+
+            <h3 className="text-xl font-bold mb-4">Your Health and Safety is Paramount:</h3>
+            <p className="text-gray-700 mb-8">
+                At S Wedding Planner, we prioritize your health and safety. Our stringent health and safety policy apply to every event we organize and manage. We adhere to legislation at every stage, from raw material procurement to transportation. Our processes are incorporated and meticulously applied. We collaborate only with contractors and suppliers equally committed to health and safety procedures. Our final checks include ensuring equipment, decor, and lighting safety and maintaining clear walkways and fire exits. All materials used are fire-retardant, and electrical equipment is PAT tested and approved. Risk assessments, action plans, and method statements are in place for a secure environment for all guests, staff, and suppliers.
+            </p>
+
+            <p className="text-gray-700">
+                We are committed to ensuring that your event is not only memorable but also safe and secure.
+            </p>
         </div>
     );
 };
 
 export default About;
+
